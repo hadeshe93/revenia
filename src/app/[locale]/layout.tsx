c391c0facbc081d63963c03e65b6d92364f9d503
hadeshe93 <hadeshe93@gmail.com>
2025-06-17 16:55:48 +0800

    feat: Updated GA4 ID

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -82,7 +82,7 @@ async function LocaleLayout({
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
-        {/* <GoogleTag /> */}
+        <GoogleTag />
       </head>
       <body>
         <NextTopLoader shadow={false} showSpinner={false} color="#0073E9" />
--- src/constants/biz.ts
+++ src/constants/biz.ts
@@ -3,7 +3,7 @@ export const DOMAIN = 'revenia.net';
 export const CAPITALIZED_DOMAIN = DOMAIN.charAt(0).toUpperCase() + DOMAIN.slice(1);
 
 // GA4
-export const GA4_ID = 'G-PZZ4JFMP7J';
+export const GA4_ID = 'G-1CB0JECE7H';
 
 // 业务 key
 export const BIZ_KEY = 'revenia';
